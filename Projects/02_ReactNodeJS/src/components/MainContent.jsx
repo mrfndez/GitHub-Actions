2474d82e9da86b3f44f782d5f1b3d17b20b2a9a5
Guillermo Fernandez <fdez.guille1@gmail.com>
2025-11-07 20:30:42 -0500

    Update MainContent.jsx

--- Projects/02_ReactNodeJS/src/components/MainContent.jsx
+++ Projects/02_ReactNodeJS/src/components/MainContent.jsx
@@ -6,7 +6,7 @@ function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
 
   function toggleHelp() {
-    setHelpVisible((isVisible)  !isVisible);
+    setHelpVisible((isVisible) => !isVisible);
   }
 
   return (
